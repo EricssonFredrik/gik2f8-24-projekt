a683565a21c2e0c01e23641dfdc9876c9515cfaf
EricssonFredrik <fredrik34943@hotmail.com>
2023-01-06 10:03:12 +0100

    Initial commit

---
+++ Projekt_packlista/client/script.js
@@ -0,0 +1,142 @@
+listForm.thing.addEventListener('keyup', (e) => validateField(e.target));
+listForm.thing.addEventListener('blur', (e) => validateField(e.target));
+
+listForm.number.addEventListener('input', (e) => validateField(e.target));
+listForm.number.addEventListener('blur', (e) => validateField(e.target));
+
+listForm.trip.addEventListener('input', (e) => validateField(e.target));
+listForm.trip.addEventListener('blur', (e) => validateField(e.target));
+
+
+listForm.addEventListener('submit', onSubmit);
+const packingListElement = document.getElementById('packingList');
+let thingValid = true;
+let numberValid = true;
+let tripValid = true;
+
+
+const api = new Api('http://localhost:5000/tasks');
+
+function validateField(field) {
+  const { name, value } = field;
+  let = validationMessage = '';
+  switch (name) {
+    case 'thing': {
+      if (value.length < 2) {
+        thingValid = false;
+        validationMessage = "Fältet 'Sak' måste innehålla minst 2 tecken.";
+      } else if (value.length > 100) {
+        thingValid = false;
+        validationMessage =
+          "Fältet 'Sak' får inte innehålla mer än 100 tecken.";
+      } else {
+        thingValid = true;
+      }
+      break;
+    }
+    case 'number': {
+      if (value.length < 1) {
+        numberValid = false;
+        validationMessage = "Fältet 'Antal' måste innehålla minst 1 tecken.";
+      } else if (value.length > 1000) {
+        numberValid = false;
+        validationMessage =
+          "Fältet 'Antal' får inte innehålla mer än 100 tecken.";
+      } else {
+        numberValid = true;
+      }
+      break;
+    }
+    case 'trip': {
+      if (value.length < 2) {
+        tripValid = false;
+        validationMessage = "Fältet 'Resa' måste innehålla minst 2 tecken.";
+      } else if (value.length > 100) {
+        tripValid = false;
+        validationMessage =
+          "Fältet 'Resa' får inte innehålla mer än 100 tecken.";
+      } else {
+        tripValid = true;
+      }
+      break;
+    }
+  }
+  
+  field.previousElementSibling.innerText = validationMessage;
+  field.previousElementSibling.classList.remove('hidden');
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  if (thingValid && numberValid && tripValid) {
+    console.log('Submit');
+    saveThing();
+  }
+}
+
+function saveThing() {
+  const thing = {
+    thing: listForm.thing.value,
+    number: listForm.number.value,
+    trip: listForm.trip.value,
+  };
+  
+
+  api.create(thing).then((thing) => {
+    if (thing) {
+      renderList();
+    }
+  });
+}
+
+
+// I denna funktion så printar vi ut listan i frontend genom att anropa metoden getAll som är vår get-metod. Och vi sorterar listan i bokstavsordning från a - ö.
+// If satsen förklarar vi efter renderThing.
+// Forts... när parametern things och längden på things är större än 0 så har vi en for each loop som ittererar genom listan
+// och då kallar på renderThings och skapar dessa div-ar när man klickar på Lägg till knappen och på så sätt sker detta dynamiskt.
+// Och bara för att visa att spara-knappen är länkad med renderList-funktionen så kan vi visa det här ovan.
+function renderList() {
+  console.log('rendering');
+  api.getAll().then((things) => {
+    packingListElement.innerHTML = '';
+
+    things.sort((p1, p2) =>
+      p2.trip < p1.trip ? 1 : p2.trip > p1.trip ? -1 : 0
+    );
+
+    if (things && things.length > 0) {
+      things.forEach((thing) => {
+        packingListElement.insertAdjacentHTML('beforeend', renderThing(thing));
+      });
+    }
+  });
+}
+
+
+// I denna funktion har vi fyra parametrar, varav 3 är våra labels med samma variabelnamn.
+//Det som sker här är att vi skapar fler olika div-ar med styling och dessa div-ar anropas i if satsen ovan. Så det som sker i if-satsen är...
+function renderThing({ id, thing, number, trip}) {
+let html = `
+<li class="select-none mt-2 py-2 border-b border-teal-300">
+  <div class="flex things-center">
+    <h3 class="mb-3 flex-1 text-xl font-bold text-zinc-700 uppercase">${thing}</h3>
+      <div>
+      <button onclick="deleteThing(${id})" class="inline-block bg-zinc-300 text-xs text-teal-900 border border-white px-3 py-1 rounded-md ml-2 hover:bg-zinc-500">Ta bort</button>
+    </div>
+  </div>
+  <div class="flex things-center">
+    <p class="ml-8 mt-2 text-xs italic basis-1/3">${number}</p>
+    <p class="ml-8 mt-2 text-xs italic basis-1/3">${trip}</p>
+  </div>`;
+return html;
+}
+
+
+function deleteThing(id) {
+  api.remove(id).then(() => {
+    renderList();
+  });
+}
+
+
+renderList();
---
+++ Projekt_packlista/server/app.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const app = express();
+const fs = require('fs/promises');
+const PORT = 5000;
+
+
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+  });
+
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await fs.readFile('./tasks.json');
+    res.send(JSON.parse(tasks));
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+
+// // // // Uppgift 2b; Här visar vi hur vi skickar data till backend
+
+/* Metod för att lyssna efter POST-anrop.*/
+app.post('/tasks', async (req, res) => {
+  try {
+        /* Alla data från klienten finns i req-objektet. I req.body finns själva innehållet i förfrågan. */
+    const task = req.body;
+        /* Det befintliga innehållet i filen läses in och sparas till variabeln listBuffer. */
+    const listBuffer = await fs.readFile('./tasks.json');
+    /* Innehållet i filen är de uppgifter som hittills är sparade. För att kunna behandla listan av uppgifter i filen som JavaScript-objekt, behövs JSON.parse.
+    Vi parsar json-filen så att vi kan köra innehållet i Javasript. */
+    const currentTasks = JSON.parse(listBuffer);
+        /* Skapar en variabel för att kunna sätta id på den senast uppladdade uppgiften */
+    let maxTaskId = 1;
+        /* Om det finns några uppgifter sedan tidigare som existerar och är en lista med en längd större än 0 ska ett nytt id räknas ut. */
+    if (currentTasks && currentTasks.length > 0) {
+            /* Det görs genom array.reduce() som går igenom alla element i listan och tar fram det högsta id:t. Det högsta id:t sparas sedan i variabeln maxTaskId */
+      maxTaskId = currentTasks.reduce(
+          /* För varje element i currentTasks anropas en callbackfunktion som får två parametrar, maxId och currentElement. maxId kommer att innehålla det id som för närvarande är 
+        högst och currentElement representerar det aktuella element i currentTasks som man för närvarande kontrollerar. */
+        (maxId, currentElement) =>
+             /* ********ÄNDRA DENNA DEL************ Om id:t för den aktuella uppgiften är större än det i variabeln maxId, sätts maxId om till det id som nu är högst. maxId är från början satt till värdet av maxTaskId 
+          (1, enligt rad 53.).  */
+          currentElement.id > maxId ? currentElement.id : maxId,
+        maxTaskId
+      );
+    }
+
+
+    /* En ny uppgift skapas baserat på den uppgift som skickades in och som hämtades ur req.body, samt egenskapen id som sätts till det högsta id av de uppgifter som redan finns 
+    (enligt uträkning med hjälp av reduce ovan) plus ett. Det befintliga objektet och det nya id:t slås ihop till ett nytt objekt med hjälp av spreadoperatorn ... */
+    const newTask = { id: maxTaskId + 1, ...task };
+    
+    /* Om currentTasks finns - dvs det finns tidigare lagrade uppgifter,  skapas en ny array innehållande tidigare uppgifter (varje befintlig uppgift i currentTasks läggs till i den 
+      nya arrayen med hjälp av spreadoperatorn) plus den nya uppgiften. Om det inte tidigare finns några uppgifter, skapas istället en ny array med endast den nya uppgiften.  */
+    const newList = currentTasks ? [...currentTasks, newTask] : [newTask];
+    /* Den nya listan görs om till en textsträng med hjälp av JSON.stringify och sparas ner till filen tasks.json med hjälp av fs-modulens writeFile-metod. Anropet är asynkront så 
+    await används för att invänta svaret innan koden går vidare. */
+
+    await fs.writeFile('./tasks.json', JSON.stringify(newList));
+
+
+
+    // // // // Uppgift 2c; Kan det vara denna? En respons från backend på att ett task har mottagits?
+
+    /* Här använder vi responsparametern däe vi skickar new task, och om det skulle bli fel så skickas ett errormeddelande samt information om felet */
+    res.send(newTask);
+  } catch (error) {
+    res.status(500).send({ error: error.stack });
+  }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  console.log(req);
+  try {
+    const id = req.params.id;
+    const listBuffer = await fs.readFile('./tasks.json');
+    const currentTasks = JSON.parse(listBuffer);
+    if (currentTasks.length > 0) {
+      
+      await fs.writeFile(
+        './tasks.json',
+        JSON.stringify(currentTasks.filter((task) => task.id != id))
+      );
+      res.send({ message: `Uppgift med id ${id} togs bort` });
+    } else {
+      res.status(404).send({ error: 'Ingen uppgift att ta bort' });
+    }
+  } catch (error) {
+    res.status(500).send({ error: error.stack });
+  }
+});
+
+app.listen(PORT, () => console.log('Server running on http://localhost:5000'));
